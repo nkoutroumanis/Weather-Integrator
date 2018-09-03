be82864a0747a0ec6eab2a86cbf220cbeed99b9b
Nicholas Koutroumanis <nikos_koutroumanis@hotmail.com>
2018-09-03 17:30:45 +0300

    BenchmarkTest class changed

--- src/test/java/com/github/nkoutroumanis/grib/BenchmarksTest.java
+++ src/test/java/com/github/nkoutroumanis/grib/BenchmarksTest.java
@@ -1,11 +1,12 @@
 package com.github.nkoutroumanis.grib;
 
 import com.github.nkoutroumanis.WeatherIntegrator;
-import org.junit.Test;
+import com.github.nkoutroumanis.lru.LRUCache;
+import com.github.nkoutroumanis.lru.LRUCacheManager;
 import org.openjdk.jmh.annotations.*;
-import org.openjdk.jmh.infra.Blackhole;
 
 import java.util.Arrays;
+import java.util.Collections;
 import java.util.concurrent.TimeUnit;
 
 
@@ -22,19 +23,35 @@ public class BenchmarksTest {
             "/Users/nicholaskoutroumanis/Desktop/folder/", "./grib_files", 4,
             9, 8, "dd/MM/yyyy hh:mm:ss",
             Arrays.asList("Relative_humidity_height_above_ground", "Temperature_height_above_ground"))
-            .clearExportingFiles().useIndex().build();
+            .clearExportingFiles().build();
 
     @Benchmark
     @BenchmarkMode(Mode.Throughput)
     @OutputTimeUnit(TimeUnit.SECONDS)
-    public void integrateDataUsingIndex(Blackhole bh) {
+    public void integrateDataUsingIndex() {
         wiWithIndex.IntegrateData();
     }
 
     @Benchmark
     @BenchmarkMode(Mode.Throughput)
     @OutputTimeUnit(TimeUnit.SECONDS)
-    public void integrateDataWithoutIndex(Blackhole bh) {
+    public void integrateDataWithoutIndex() {
         wiWithoutIndex.IntegrateData();
     }
+
+    @Benchmark
+    @BenchmarkMode(Mode.Throughput)
+    @OutputTimeUnit(TimeUnit.SECONDS)
+    public GribFilesTree gribTreeConstruction() {
+        return GribFilesTree.newGribFilesTree("./grib_files", ".grb2");
+    }
+
+    @Benchmark
+    @BenchmarkMode(Mode.Throughput)
+    @OutputTimeUnit(TimeUnit.SECONDS)
+    public LRUCacheManager overallPreProcessing() {
+        return LRUCacheManager.newLRUCacheManager(GribFilesTree.newGribFilesTree("./grib_files", ".grb2"),
+                LRUCache.newLRUCache(4), true, Collections.unmodifiableList(Arrays.asList("Temperature_height_above_ground")), ";");
+    }
+
 }
