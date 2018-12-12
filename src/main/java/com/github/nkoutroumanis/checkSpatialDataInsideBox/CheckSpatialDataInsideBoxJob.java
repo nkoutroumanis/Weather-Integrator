b4aac4cc3129fcea38e9b00cdffb7529875a46f1
Nicholas Koutroumanis <nikos_koutroumanis@hotmail.com>
2018-12-13 00:00:03 +0200

    number changed

--- src/main/java/com/github/nkoutroumanis/checkSpatialDataInsideBox/CheckSpatialDataInsideBoxJob.java
+++ src/main/java/com/github/nkoutroumanis/checkSpatialDataInsideBox/CheckSpatialDataInsideBoxJob.java
@@ -8,7 +8,7 @@ public class CheckSpatialDataInsideBoxJob {
 //        CheckSpatialDataInsideBox.newCheckSpatioTemporalInfo("/home/nikolaos/Documents/tambak",
 //                2, 3).build().exportTxt("/home/nikolaos/Documents/gb");
 
-        CheckSpatialDataInsideBox.newCheckSpatioTemporalInfo(args[0], Integer.valueOf(args[1]), Integer.valueOf(args[2]), Space2D.newSpace2D(-31.2686628, 32.6340432, 46.6703883, 80.8218858)).build().exportTxt(args[4]);
+        CheckSpatialDataInsideBox.newCheckSpatioTemporalInfo(args[0], Integer.valueOf(args[1]), Integer.valueOf(args[2]), Space2D.newSpace2D(-31.2686628, 32.6340432, 46.6703883, 80.8218858)).build().exportTxt(args[3]);
 
 
     }
