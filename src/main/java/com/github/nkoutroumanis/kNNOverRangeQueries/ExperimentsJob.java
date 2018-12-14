d781e7248783bcd528d2b2ee3bece2cbfeaefd05
Nicholas Koutroumanis <nikos_koutroumanis@hotmail.com>
2018-12-15 00:36:02 +0200

    ExperimentsJob modified

--- src/main/java/com/github/nkoutroumanis/kNNOverRangeQueries/ExperimentsJob.java
+++ src/main/java/com/github/nkoutroumanis/kNNOverRangeQueries/ExperimentsJob.java
@@ -12,6 +12,7 @@ import java.io.*;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
+import java.text.DecimalFormat;
 import java.util.*;
 import java.util.stream.Stream;
 
@@ -26,7 +27,9 @@ public class ExperimentsJob {
         MongoCollection m = mongoClient.getDatabase("test").getCollection("geoPoints");
         Random r = new Random();
 
-                List<File> subfolder = Arrays.asList(new File(args[0]).listFiles(File::isDirectory)) ;
+        DecimalFormat dec = new DecimalFormat("#0.00");
+
+                List<File> subfolder = Arrays.asList(new File(args[0]/*"/Users/nicholaskoutroumanis/Desktop/untitled/"*/).listFiles(File::isDirectory)) ;
 
                 subfolder.forEach(path -> {
 
@@ -35,11 +38,11 @@ public class ExperimentsJob {
                     LoadHistogram lh = LoadHistogram.newLoadHistogram(path.toString());
                     RadiusDetermination rd = RadiusDetermination.newRadiusDetermination(lh.getHistogram(), lh.getNumberOfCellsxAxis(), lh.getNumberOfCellsyAxis(), lh.getMinx(), lh.getMiny(), lh.getMaxx(), lh.getMaxy());
 
-                    int k = Integer.valueOf(args[1]);
+                    final int k = Integer.valueOf(args[1]);
                     int points = 1000;
 
                     List<Long> timeForRadiusDetermination = new ArrayList<>();
-                    List<Long> resultsRatio = new ArrayList<>();
+                    List<Double> resultsRatio = new ArrayList<>();
                     List<Double> radiusRatio = new ArrayList<>();
 
                     for(int i = 0; i< points;i++){
@@ -47,29 +50,39 @@ public class ExperimentsJob {
                         double randomX = lh.getMinx() + ((lh.getMaxx() - 0.1d) - lh.getMinx()) * r.nextDouble();
                         double randomY = lh.getMiny() + ((lh.getMaxy() - 0.1) - lh.getMiny()) * r.nextDouble();
 
+                        randomX = Double.parseDouble(dec.format(randomX));
+                        randomY = Double.parseDouble(dec.format(randomY));
+
+
                         long t1 = System.currentTimeMillis();
                         double determinedRadius = rd.findRadius(randomX, randomY, Long.valueOf(k));
                         timeForRadiusDetermination.add(System.currentTimeMillis()-t1);
 
+                        System.out.println("Point :"+randomX +"  "+randomY +" Radius"+determinedRadius);
+
                         MongoCursor<Document> cursor1 = m.aggregate(Arrays.asList(Document.parse( "{ $match: { location: { $geoWithin : { $centerSphere : [ ["+randomX+", "+randomY+"], "+(determinedRadius/6378.1)+" ] } } } }"),Document.parse("{ $count: \"count\" }"))).iterator();
-                        resultsRatio.add(Long.valueOf(cursor1.next().getInteger("count")-k));//(n' - n)/n
+                        resultsRatio.add(( (double) (cursor1.next().getInteger("count")-k)/k ));//(n' - n)/n
                         cursor1.close();
+                        System.out.println(resultsRatio.size()+" Count Finished");
 
                         if(resultsRatio.get(resultsRatio.size()-1)<0){
                             try {
-                                throw new Exception("Negateive numbers are added in the list");
+                                System.out.println("Error:"+resultsRatio.get(resultsRatio.size()-1));
+                                throw new Exception("Negative numbers are added in the list");
                             } catch (Exception e) {
                                 e.printStackTrace();
                             }
                         }
 
                         MongoCursor<Document> cursor2 = m.aggregate(Arrays.asList(Document.parse( "{ $geoNear: { near: {type: \"Point\", coordinates: ["+randomX+", "+randomY+"]}," +
-                                "key: \"location\" ," + "maxDistance: "+ ((( determinedRadius)) *1000l) +" ," + "distanceField: \"distance\" ," + "spherical: true, num:"+k + "} }"),Document.parse("{ $group: { _id:null, theLast:{ $last:\"$distance\" } } }"))).iterator();
-                        radiusRatio.add(determinedRadius-cursor2.next().getDouble("theLast"));//(r' - r)/r
+                                "key: \"location\" ," + "maxDistance: "+ (((determinedRadius)) *1000) +" ," + "distanceField: \"distance\" ," + "spherical: true, num:"+k + "} }"),Document.parse("{ $group: { _id:null, theLast:{ $last:\"$distance\" } } }"))).iterator();
+                        radiusRatio.add((determinedRadius*1000)-cursor2.next().getDouble("theLast"));//(r' - r)/r
                         cursor2.close();
+                        System.out.println("Radius Finished");
 
                         if(radiusRatio.get(radiusRatio.size()-1)<0){
                             try {
+                                System.out.println("Error: "+radiusRatio.get(resultsRatio.size()-1));
                                 throw new Exception("Negative numbers are added in the list");
                             } catch (Exception e) {
                                 e.printStackTrace();
@@ -85,10 +98,10 @@ public class ExperimentsJob {
                     }
                     double tssStd = Math.sqrt(tsum/(timeForRadiusDetermination.size()-1));
 
-                    LongSummaryStatistics ress = resultsRatio.stream().mapToLong(Long::valueOf).summaryStatistics();
+                    DoubleSummaryStatistics ress = resultsRatio.stream().mapToDouble(Double::valueOf).summaryStatistics();
                     double resum = 0;
-                    for(Long e:resultsRatio){
-                        resum = resum + Math.pow(e.doubleValue() - ress.getAverage(),2);
+                    for(Double e:resultsRatio){
+                        resum = resum + Math.pow(e - ress.getAverage(),2);
                     }
                     double ressStd = Math.sqrt(resum/(resultsRatio.size()-1));
 
