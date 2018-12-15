2317f981d9daabe96b48f02538077b7e79c6b6dc
Nicholas Koutroumanis <nikos_koutroumanis@hotmail.com>
2018-12-15 13:41:42 +0200

    ExperimentsJob modified

--- src/main/java/com/github/nkoutroumanis/histogram/HistogramCreationJob.java
+++ src/main/java/com/github/nkoutroumanis/histogram/HistogramCreationJob.java
@@ -2,7 +2,7 @@ package com.github.nkoutroumanis.histogram;
 
 public final class HistogramCreationJob {
     public static void main(String args[]) {
-        //x for longitude, y for latitude- the max values of lon and lat should be increased a little in order to include the whole data in histogram
+        //x for longitude, y for latitude - the max values of lon and lat should be increased a little in order to include the whole data in histogram
         Space2D space = Space2D.newSpace2D(-26.64, 0, 121.57, 59.94);
         long t1;
         int j = 1;
