d258e231e5754d2acff8e714d0f1d87ce8a29bd7
Panagiotis Nikitopoulos <nikp@unipi.gr>
2019-05-06 17:23:02 +0300

    Added logging mechanism

---
+++ src/main/java/com/github/nkoutroumanis/kafkaToMongoDB/KafkaToMongoJob.java
@@ -0,0 +1,13 @@
+package com.github.nkoutroumanis.kafkaToMongoDB;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class KafkaToMongoJob {
+
+    private static final Logger logger = LoggerFactory.getLogger(KafkaToMongoJob.class);
+
+    public static void main(String[] args) {
+        logger.info("test");
+    }
+}
