22ae0d782e86c6cee344bfb470ac07f7334bbed9
nikolaos <nikos_koutroumanis@hotmail.com>
2019-05-16 17:21:28 +0300

    modification were made

--- src/main/java/com/github/nkoutroumanis/filesSampling/FilesSamplingJob.java
+++ src/main/java/com/github/nkoutroumanis/filesSampling/FilesSamplingJob.java
@@ -1,7 +1,19 @@
 package com.github.nkoutroumanis.filesSampling;
 
-public class FilesSamplingJob {
-    public static void main(String args[]){
+import com.github.nkoutroumanis.datasources.Datasource;
+import com.github.nkoutroumanis.datasources.FileDatasource;
+import com.github.nkoutroumanis.outputs.FileOutput;
+import com.github.nkoutroumanis.parsers.CsvRecordParser;
+import com.github.nkoutroumanis.parsers.RecordParser;
 
+import java.io.IOException;
+
+public class FilesSamplingJob {
+    public static void main(String args[]) throws Exception {
+        Datasource ds = FileDatasource.newFileDatasource("/home/nikolaos/Documents/thesis-dataset/", ".csv");
+        RecordParser rp = new CsvRecordParser(ds, ";", 2, 3, 4, "yyyy-MM-dd HH:mm:ss");
+        FileOutput fileOutput = FileOutput.newFileOutput("/home/nikolaos/Documents/thesis-dataset-sampling/", true);
+
+        FilesSamping.newFilesSamping(rp, 1000000).build().exportSamplesToFile(fileOutput);
     }
 }
