2e94d3ac3e96437e3114ca68568cda23b6571e98
Nicholas Koutroumanis <nikos_koutroumanis@hotmail.com>
2019-07-14 20:34:19 +0300

    weather integrator reading grib files from HDFS second commint

--- src/main/java/com/github/nkoutroumanis/weatherIntegrator/WeatherDataObtainer.java
+++ src/main/java/com/github/nkoutroumanis/weatherIntegrator/WeatherDataObtainer.java
@@ -34,7 +34,7 @@ public final class WeatherDataObtainer {
                     e.printStackTrace();
                 }
 
-                return org.dia.utils.NetCDFUtils.loadDFSNetCDFDataSet("hdfs://"+uri.getAuthority()+ "/", uri.getPath(), JobFilesUsingIndex.BUFFERSIZE, false);
+                return org.dia.utils.NetCDFUtils.loadDFSNetCDFDataSet("hdfs://"+uri.getAuthority()+ "/", uri.getPath().substring(1), JobFilesUsingIndex.BUFFERSIZE, false);
             };
 
             URI uri = new URI(gribFilesFolderPath);
--- src/test/java/com/github/nkoutroumanis/weatherIntegrator/WeatherDataObtainerTest.java
+++ src/test/java/com/github/nkoutroumanis/weatherIntegrator/WeatherDataObtainerTest.java
@@ -17,7 +17,7 @@ public class WeatherDataObtainerTest {
 //
 //        System.out.println(s.substring(7).substring(s.substring(7).indexOf("/")));
 
-URI uri = new URI("hdfs://localhost:50070/gfgf");
+URI uri = new URI("hdfs://localhost:50070/gfgf/sdsds");
 
 
         System.out.println("hdfs://"+uri.getAuthority()+ "/");
