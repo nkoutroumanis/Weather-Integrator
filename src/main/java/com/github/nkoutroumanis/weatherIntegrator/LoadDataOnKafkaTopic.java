ffaa9a1716e34af7044c40bf26e7b7d144f57cc3
nikolaos <nikos_koutroumanis@hotmail.com>
2019-07-22 15:18:41 +0300

    Classes modified

---
+++ src/main/java/com/github/nkoutroumanis/weatherIntegrator/LoadDataOnKafkaTopic.java
@@ -0,0 +1,29 @@
+package com.github.nkoutroumanis.weatherIntegrator;
+
+import com.github.nkoutroumanis.datasources.FileDatasource;
+import com.github.nkoutroumanis.outputs.KafkaOutput;
+import com.github.nkoutroumanis.parsers.CsvRecordParser;
+import com.github.nkoutroumanis.parsers.Record;
+
+import java.io.IOException;
+import java.text.ParseException;
+
+public class LoadDataOnKafkaTopic {
+
+    public static void main(String args[]) throws IOException, ParseException {
+
+        FileDatasource fileDatasource = FileDatasource.newFileDatasource("/home/user/vehicles/",".csv");
+        CsvRecordParser csvRecordParser = new CsvRecordParser(fileDatasource,";", null);
+        KafkaOutput kafkaOutput = KafkaOutput.newKafkaOutput("./producer.properties","vehiclesWithoutWeather");
+
+        while(csvRecordParser.hasNextRecord()){
+
+            Record record = csvRecordParser.nextRecord();
+            record.deleteLastFieldValue();
+
+            kafkaOutput.out(csvRecordParser.toCsv(record,";"),"");
+        }
+        kafkaOutput.close();
+        fileDatasource.cloneDatasource();
+    }
+}
