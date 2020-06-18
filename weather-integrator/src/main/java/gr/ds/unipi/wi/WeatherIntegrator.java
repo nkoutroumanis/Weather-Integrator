2adf706a50cae244774eec600b3616a357357b42
Nicholas Koutroumanis <nikos_koutroumanis@hotmail.com>
2020-06-19 01:37:36 +0300

    weather integration date function modification

--- weather-integrator/src/main/java/gr/ds/unipi/wi/WeatherIntegrator.java
+++ weather-integrator/src/main/java/gr/ds/unipi/wi/WeatherIntegrator.java
@@ -108,20 +108,22 @@ public final class WeatherIntegrator {
 
         start = System.currentTimeMillis();
 
-        Function<Record, Date> dateFunction;
-        if(recordParser.getDateFormat().equals("unixTimestampSec")){
-            dateFunction = RecordParser.dateFunctionUnixTimestampSec(recordParser);
-        }
-        else if(recordParser.getDateFormat().equals("unixTimestampMillis")){
-            dateFunction = RecordParser.dateFunctionUnixTimestampMillis(recordParser);
-        }
-        else if(recordParser.getDateFormat().equals("unixTimestampDecimals")){
-            dateFunction = RecordParser.dateFunctionUnixTimestampDecimals(recordParser);
-        }
-        else{
-            recordParser.setSimpleDateFormat();
-            dateFunction = RecordParser.dateFunctionDateFormatPattern(recordParser);
-        }
+        Function<Record, Date> dateFunction = recordParser.getDateFunction();
+
+//        Function<Record, Date> dateFunction;
+//        if(recordParser.getDateFormat().equals("unixTimestampSec")){
+//            dateFunction = RecordParser.dateFunctionUnixTimestampSec(recordParser);
+//        }
+//        else if(recordParser.getDateFormat().equals("unixTimestampMillis")){
+//            dateFunction = RecordParser.dateFunctionUnixTimestampMillis(recordParser);
+//        }
+//        else if(recordParser.getDateFormat().equals("unixTimestampDecimals")){
+//            dateFunction = RecordParser.dateFunctionUnixTimestampDecimals(recordParser);
+//        }
+//        else{
+//            recordParser.setSimpleDateFormat();
+//            dateFunction = RecordParser.dateFunctionDateFormatPattern(recordParser);
+//        }
 
         long window = 0;
         long startTimeWindow = System.currentTimeMillis();
